381e12b87052a120141f71755dbab855c542e0a2
Pilow <pierre.laupretre@gmail.com>
2019-03-06 23:43:28 +0100

    [include] Add more tests with files

--- src/test/java/test/TestWithFile.java
+++ src/test/java/test/TestWithFile.java
@@ -1,6 +1,9 @@
 package test;
 
+import leekscript.compiler.AIFile;
 import leekscript.compiler.LeekScript;
+import leekscript.compiler.resolver.ResolverContext;
+import leekscript.compiler.resolver.Resolver;
 
 import static org.junit.Assert.assertEquals;
 
@@ -21,5 +24,33 @@ public class TestWithFile {
 	@Test
 	public void testInclude() throws Exception {
 		assertEquals("[a, b, KEY]", LeekScript.runFile("test/ai/array_keys.leek"));
+	}
+	
+	@Test
+	public void testIncludeMultiple() throws Exception {
+		assertEquals("[a, b, KEY]", LeekScript.runFile("test/ai/include_multiple.leek"));
+	}
+	
+	@Test
+	public void testCustomResolver() throws Exception {
+		class CustomContext extends ResolverContext {} 
+		LeekScript.setResolver(new Resolver<CustomContext>() {
+			@Override
+			public AIFile<CustomContext> resolve(String path, ResolverContext context) {
+				return new AIFile<CustomContext>(path, "return 'generated';", new CustomContext());
+			}
+		});
+		assertEquals("generated", LeekScript.runFile("whatever"));
+		LeekScript.resetResolver();
+	}
+	
+	@Test
+	public void testSubFolder() throws Exception {
+		assertEquals("sub", LeekScript.runFile("test/ai/include_sub.leek"));
+	}
+	
+	@Test
+	public void testRelativePath() throws Exception {
+		assertEquals("cent-vingt-trois millions quatre-cent-cinquante-six-mille-sept-cent-quatre-vingt-neuf", LeekScript.runFile("test/ai/subfolder/include_parent.leek"));
 	}
 }
