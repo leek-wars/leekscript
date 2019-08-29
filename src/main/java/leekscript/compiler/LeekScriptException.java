7754c9deb867a955cd4a53818b2cb723bd92b3ad
Pilow <pierre.laupretre@gmail.com>
2019-08-29 13:06:16 +0200

    [compilation] Improve java compilation process and errors
    Signed-off-by: Pilow <pierre.laupretre@gmail.com>

--- src/main/java/leekscript/compiler/LeekScriptException.java
+++ src/main/java/leekscript/compiler/LeekScriptException.java
@@ -9,22 +9,23 @@ public class LeekScriptException extends Exception {
 	public final static int CODE_TOO_LARGE_FUNCTION = 2;
 
 	private final int mType;
-	private String mFunction = null;
+	private String mMessage = null;
 
 	public LeekScriptException(int type) {
 		mType = type;
 	}
 
-	public LeekScriptException(int type, String function) {
+	public LeekScriptException(int type, String message) {
 		mType = type;
-		mFunction = function;
+		mMessage = message;
 	}
 
 	public int getType() {
 		return mType;
 	}
 
-	public String getFunction() {
-		return mFunction;
+	@Override
+	public String getMessage() {
+		return mMessage;
 	}
 }
