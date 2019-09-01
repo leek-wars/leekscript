319e7c8ec563a7b0dcaf338eda2d29b630f2042f
Pilow <pierre.laupretre@gmail.com>
2019-09-01 02:51:36 +0200

    [function] Merge addFunctionOperations and addVariableOperations

--- src/main/java/leekscript/functions/Functions.java
+++ src/main/java/leekscript/functions/Functions.java
@@ -11,12 +11,11 @@ public class Functions {
 
 	private static Map<String, VariableOperations> sVariableOperations = new TreeMap<String, VariableOperations>();
 
-	public static void addFunctionOperations(String function, int operations) {
-		LeekFunctions.getValue(function).setOperations(operations);
-	}
-
-	public static void addVariableOperations(String name, String variableOperations) {
-		sVariableOperations.put(name, new VariableOperations(JSONObject.parseObject(variableOperations)));
+	public static void addFunctionOperations(String function, int operations, String variableOperations) {
+		LeekFunctions.getValue(function).setOperations(Math.max(1, operations));
+		if (variableOperations != null) {
+			sVariableOperations.put(function, new VariableOperations(JSONObject.parseObject(variableOperations)));
+		}
 	}
 
 	public static VariableOperations getVariableOperations(String name) {
