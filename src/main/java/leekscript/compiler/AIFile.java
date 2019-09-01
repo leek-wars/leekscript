972bd801a404c3c5341f90add068f62bc5d69fca
Pilow <pierre.laupretre@gmail.com>
2019-09-01 15:20:09 +0200

    [aifile] Add an id attribute to AIFile (used by Leek Wars generator)

--- src/main/java/leekscript/compiler/AIFile.java
+++ src/main/java/leekscript/compiler/AIFile.java
@@ -8,11 +8,24 @@ public class AIFile<C extends ResolverContext> {
 	private String code;
 	private C context;
 	private String javaClassName;
-	
+	private int id;
+
 	public AIFile(String path, String code, C context) {
 		this.path = path;
 		this.code = code;
 		this.context = context;
+	}
+	public AIFile(String path, String code, C context, int id) {
+		this.path = path;
+		this.code = code;
+		this.context = context;
+		this.id = id;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
 	}
 	public String getCode() {
 		return code;
