f2b1fb7645ba0b37c29f34d8148bd0c4d8000f13
Pilow <pierre.laupretre@gmail.com>
2020-02-05 20:12:17 +0100

    [top-level] Add ability to run a leekscript file

--- src/main/java/leekscript/TopLevel.java
+++ src/main/java/leekscript/TopLevel.java
@@ -1,6 +1,7 @@
 package leekscript;
 
 import java.util.Scanner;
+import java.io.File;
 
 import leekscript.compiler.LeekScript;
 import leekscript.runner.AI;
@@ -12,35 +13,60 @@ public class TopLevel {
 		if (args.length < 1) {
 			Scanner input = new Scanner(System.in);
 		    System.out.print(">>> ");
-	
 		    String code;
 		    while ((code = input.nextLine()) != null) {
-		    	execute(code);
+		    	executeSnippet(code);
 		    	System.out.print(">>> ");
 			}
 	    	input.close();
 		} else {
-			String code = args[0];
-			execute(code);
+
+			File file = new File(args[0]);
+			if (file.exists()) {
+				executeFile(file);
+			} else {
+				executeSnippet(args[0]);
+			}
 		}
 	}
-	
-	private static void execute(String code) {
+
+	private static void executeSnippet(String code) {
 		try {
 			long ct = System.currentTimeMillis();
 			AI ai = LeekScript.compileSnippet(code, "AI", "leekscript.jar");
 			long compileTime = System.currentTimeMillis() - ct;
-			
+
 			long et = System.currentTimeMillis();
 			AbstractLeekValue v = ai.runIA();
 			long executionTime = System.currentTimeMillis() - et;
-			
+
 			String result = v.getString(ai);
 			long ops = ai.getOperations();
-			
+
 			System.out.println(result);
 			System.out.println("(" + ops + " ops, " + compileTime + "ms + " + executionTime + "ms)");
-			
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	private static void executeFile(File file) {
+		try {
+			long ct = System.currentTimeMillis();
+			AI ai = LeekScript.compileFile(file.getPath(), "AI", "leekscript.jar", false);
+			long compileTime = System.currentTimeMillis() - ct;
+
+			long et = System.currentTimeMillis();
+			AbstractLeekValue v = ai.runIA();
+			long executionTime = System.currentTimeMillis() - et;
+
+			String result = v.getString(ai);
+			long ops = ai.getOperations();
+
+			System.out.println(result);
+			System.out.println("(" + ops + " ops, " + compileTime + "ms + " + executionTime + "ms)");
+
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
