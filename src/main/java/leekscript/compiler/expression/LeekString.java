df5012c2798aec7def9edc0f8c67b51998670711
Pilow <pierre.laupretre@gmail.com>
2021-02-13 11:36:42 +0100

    [string] Fix \\ escaping in LS 1.1

--- src/main/java/leekscript/compiler/expression/LeekString.java
+++ src/main/java/leekscript/compiler/expression/LeekString.java
@@ -31,13 +31,20 @@ public class LeekString extends AbstractExpression {
 	public void writeJavaCode(MainLeekBlock mainblock, JavaWriter writer) {
 		String str = "";
 		int len = mString.length() - 1;
-		for(int i = 0; i < mString.length(); i++){
-			if(mString.charAt(i) == '\n') str += "\\n";
-			else if(mString.charAt(i) == '"') str += "\\\"";
-			else if(mString.charAt(i) == '\\'){
-				if(len > i && mString.charAt(i + 1) == 'n') str += "\\";
-				else if(len > i && mString.charAt(i + 1) == 't') str += "\\";
-				else str += "\\\\";
+		for (int i = 0; i < mString.length(); i++) {
+			if (mString.charAt(i) == '\n') str += "\\n";
+			else if (mString.charAt(i) == '"') str += "\\\"";
+			else if (mString.charAt(i) == '\\') {
+				if (len > i && mString.charAt(i + 1) == 'n') str += "\\";
+				else if (len > i && mString.charAt(i + 1) == 't') str += "\\";
+				else {
+					if (mainblock.getCompiler().getCurrentAI().getVersion() >= 11) {
+						str += "\\";
+					} else {
+						// LeekScript 1.0 had a bug with "\\" strings producing 4 \
+						str += "\\\\";
+					}
+				}
 			}
 			else str += mString.charAt(i);
 		}
