9c3aad157fd035a74bcbaff49cb351292536d332
Pilow <pierre.laupretre@gmail.com>
2021-02-20 23:27:38 +0100

    [global] Don't check global variables in v1.0 (issue with includes)

--- src/main/java/leekscript/compiler/instruction/LeekVariableDeclarationInstruction.java
+++ src/main/java/leekscript/compiler/instruction/LeekVariableDeclarationInstruction.java
@@ -81,9 +81,10 @@ public class LeekVariableDeclarationInstruction implements LeekInstruction {
 			compiler.addError(new AnalyzeError(token, AnalyzeErrorLevel.ERROR, LeekCompilerException.THIS_NOT_ALLOWED_HERE));
 		} else {
 			// Vérification déjà existante (on vérifie les globales seulement en 1.1 car il y a un léger bug en 1.0 avec les includes)
-			if (compiler.getVersion() >= 11 && (compiler.getMainBlock().hasGlobal(token.getWord()) || compiler.getMainBlock().hasUserFunction(token.getWord(), true)) || compiler.getCurrentBlock().hasVariable(token.getWord())) {
+			if ((compiler.getVersion() >= 11 && compiler.getMainBlock().hasGlobal(token.getWord())) || compiler.getMainBlock().hasUserFunction(token.getWord(), true) || compiler.getCurrentBlock().hasVariable(token.getWord())) {
 				compiler.addError(new AnalyzeError(token, AnalyzeErrorLevel.ERROR, LeekCompilerException.VARIABLE_NAME_UNAVAILABLE));
 			} else {
+				// On ajoute la variable
 				compiler.getCurrentBlock().addVariable(new LeekVariable(token, VariableType.LOCAL));
 			}
 		}
