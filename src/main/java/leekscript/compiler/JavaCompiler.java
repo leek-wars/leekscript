2fccaa3b395f2bc87d2be23c09ad8803cc309700
Pilow <pierre.laupretre@gmail.com>
2021-04-11 12:11:35 +0200

    [compiler] Use javax.tools.JavaCompiler

--- src/main/java/leekscript/compiler/JavaCompiler.java
+++ src/main/java/leekscript/compiler/JavaCompiler.java
@@ -6,6 +6,8 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 
+import javax.tools.ToolProvider;
+
 public class JavaCompiler {
 
 	public final static int INIT = 0;
@@ -28,96 +30,14 @@ public class JavaCompiler {
 			return;
 		}
 
+		javax.tools.JavaCompiler compiler = ToolProvider.getSystemJavaCompiler();
 		String classpath = JavaCompiler.class.getProtectionDomain().getCodeSource().getLocation().toURI().getPath();
-		String[] args = new String[] { "javac", "-encoding", "utf8", "-classpath", classpath, mInput.getPath() };
-		// System.out.println(String.join(" ", args));
-		ProcessBuilder pb = new ProcessBuilder(args);
-		Worker worker = new Worker(pb);
-		Process process = worker.begin();
 
-		Gobbler outGobbler = new Gobbler(process.getInputStream());
-		Gobbler errGobbler = new Gobbler(process.getErrorStream());
-		Thread outThread = new Thread(outGobbler);
-		Thread errThread = new Thread(errGobbler);
-		outThread.start();
-		errThread.start();
-
-		try {
-			worker.join(20000);
-			if (worker.exit == null) {
-				throw new CompilationException("too_long_java");
-			}
-			outThread.join();
-			errThread.join();
-			process.waitFor();
-			String error = errGobbler.getOuput();
-			if (error.length() > 0) {
-				mStatus = END;
-				throw new CompilationException(error);
-			}
-			if (process.exitValue() == 0) {
-				mStatus = END;
-			}
-		} catch (InterruptedException ex) {
-			worker.interrupt();
-			Thread.currentThread().interrupt();
-			throw ex;
-		} finally {
-			process.destroy();
-		}
+		compiler.run(null, System.out, System.out, "-encoding", "utf8", "-classpath", classpath, mInput.getPath());
+		// System.out.println("Javac result = " + result);
 	}
 
 	public static int getStatus() {
 		return 0;
 	}
-
-	public static class Worker extends Thread {
-		private final ProcessBuilder pb;
-		private Process p;
-		public Integer exit;
-		public Worker(ProcessBuilder pb) {
-			this.pb = pb;
-		}
-		public Process begin() {
-			try {
-				p = pb.start();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-			super.start();
-			return p;
-		}
-		public void run() {
-			try {
-				exit = p.waitFor();
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-		}
-	}
-
-	public static class Gobbler implements Runnable {
-        private BufferedReader reader;
-        private StringBuilder output;
-        public Gobbler(InputStream inputStream) {
-            this.reader = new BufferedReader(new InputStreamReader(inputStream));
-        }
-        public void run() {
-            String line;
-            output = new StringBuilder();
-            try {
-                while ((line = reader.readLine()) != null) {
-					// Limit the length of the line to 500 characters
-                    output.append(line.substring(0, Math.min(line.length(), 500)) + "\n");
-                }
-                reader.close();
-            } catch (IOException e) {
-				System.err.println("ERROR: " + e.getMessage());
-				e.printStackTrace();
-            }
-        }
-        public String getOuput() {
-            return this.output.toString();
-        }
-    }
 }
