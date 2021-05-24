eec4833013d0bc2c013134e2155d1dafbbe4b8a6
Pilow <pierre.laupretre@gmail.com>
2021-05-24 18:48:48 +0200

    [test] Fix tests

--- src/test/java/test/TestReference.java
+++ src/test/java/test/TestReference.java
@@ -13,7 +13,7 @@ public class TestReference extends TestCommon {
 		code_v10("var t = 0; var f = function(a) { t = @a }; var b = []; f(b); push(t, 5); return b;").equals("[]");
 		code_v10("var t = 0; var f = function(@a) { t = @a }; var b = []; f(b); push(t, 5); return b;").equals("[5]");
 		code_v10("var t = 0; var f = function(a) { t; }; f(t); return 'ok';").equals("ok");
-		code_v10("function ref() { var a = 2; return @a; } return 1 / ref();").equals("0.5");
+		code_v10("function ref() { var a = 2; return @a; } return 1 / ref();").equals("0,5");
 		code_v10("var a = @[1, 2, 3]; var b = @a; return (@(b));").equals("[1, 2, 3]");
 		code_v10("var count = count([1, 2, 3]) return count").equals("3");
 		code_v10("var a = 12; var b = @a; a++; return [a, b]").equals("[13, 12]");
