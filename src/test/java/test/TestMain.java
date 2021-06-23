425505a3cd80b2e5aba007a91a19152d56550097
Pilow <pierre.laupretre@gmail.com>
2021-06-23 11:33:42 +0200

    set locale

--- src/test/java/test/TestMain.java
+++ src/test/java/test/TestMain.java
@@ -1,10 +1,24 @@
 package test;
 
+import java.util.Locale;
+
 import org.junit.Assert;
 
 public class TestMain {
 
 	public static void main(String[] args) throws Exception {
+
+		Locale.setDefault(Locale.FRENCH);
+
+		Locale currentLocale = Locale.getDefault();
+		System.out.println("Locale = ");
+		System.out.println(currentLocale.getDisplayLanguage());
+		System.out.println(currentLocale.getDisplayCountry());
+		System.out.println(currentLocale.getLanguage());
+		System.out.println(currentLocale.getCountry());
+		System.out.println(System.getProperty("user.country"));
+		System.out.println(System.getProperty("user.language"));
+
         System.out.println("Start tests...");
 
 		// new TestCommon().code_v11("return 5 + 5;").equals("10");
