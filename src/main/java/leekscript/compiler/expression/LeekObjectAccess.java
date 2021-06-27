aca02eff79b64773a2371d48a88c7dfc3e7638bc
Pilow <pierre.laupretre@gmail.com>
2021-06-27 19:38:40 +0200

    [ops] Fix ops

--- src/main/java/leekscript/compiler/expression/LeekObjectAccess.java
+++ src/main/java/leekscript/compiler/expression/LeekObjectAccess.java
@@ -70,6 +70,7 @@ public class LeekObjectAccess extends AbstractExpression {
 					compiler.addError(new AnalyzeError(field, AnalyzeErrorLevel.ERROR, Error.CLASS_MEMBER_DOES_NOT_EXIST, new String[] { clazz.getName(), field.getWord() }));
 				}
 			} else if (v.getVariableType() == VariableType.CLASS && v.getClassDeclaration() != null) {
+				operations -= 1;
 				if (!v.getClassDeclaration().hasStaticMember(field.getWord())) {
 					compiler.addError(new AnalyzeError(field, AnalyzeErrorLevel.ERROR, Error.CLASS_STATIC_MEMBER_DOES_NOT_EXIST, new String[] { v.getClassDeclaration().getName(), field.getWord() }));
 				}
