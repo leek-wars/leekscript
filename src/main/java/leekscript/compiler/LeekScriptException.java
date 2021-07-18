3ff87fd0a36ce626cb359a2ce8ed3a762a2484d6
Pilow <pierre.laupretre@gmail.com>
2021-07-18 21:50:04 +0200

    [compiler] Fix compilation with multithreading

--- src/main/java/leekscript/compiler/LeekScriptException.java
+++ src/main/java/leekscript/compiler/LeekScriptException.java
@@ -1,31 +1,29 @@
 package leekscript.compiler;
 
+import leekscript.common.Error;
+
 public class LeekScriptException extends Exception {
 
 	private static final long serialVersionUID = -5149423928011355230L;
 
-	public final static int CANT_COMPILE = 0;
-	public final static int CODE_TOO_LARGE = 1;
-	public final static int CODE_TOO_LARGE_FUNCTION = 2;
-
-	private final int mType;
+	private final Error mType;
 	private String mMessage = null;
 
-	public LeekScriptException(int type) {
+	public LeekScriptException(Error type) {
 		mType = type;
 	}
 
-	public LeekScriptException(int type, String message) {
+	public LeekScriptException(Error type, String message) {
 		mType = type;
 		mMessage = message;
 	}
 
-	public int getType() {
+	public Error getType() {
 		return mType;
 	}
 
 	@Override
 	public String getMessage() {
-		return mMessage;
+		return mType.name() + " : " + mMessage;
 	}
 }
