4156469b32c201dabae207d40a29e49d524e64e7
Hazurl <ricoujules69@gmail.com>
2023-05-20 15:27:14 +0200

    [array] Refactor arrayvs map handling

--- src/main/java/leekscript/compiler/expression/LeekArray.java
+++ src/main/java/leekscript/compiler/expression/LeekArray.java
@@ -15,7 +15,7 @@ import leekscript.compiler.bloc.MainLeekBlock;
 
 public class LeekArray extends Expression {
 
-	private final ArrayList<Expression> mValues = new ArrayList<Expression>();
+	private final ArrayList<LeekArrayElement> mElements = new ArrayList<LeekArrayElement>();
 	public boolean mIsKeyVal = false;
 	public Type type = Type.ARRAY;
 	private Token openingBracket;
@@ -25,8 +25,8 @@ public class LeekArray extends Expression {
 		this.openingBracket = openingBracket;
 	}
 
-	public void addValue(Expression param) {
-		mValues.add(param);
+	public void addValue(Expression value) {
+		mElements.add(new LeekArrayValue(value));
 	}
 
 	public void setClosingBracket(Token closingBracket) {
@@ -36,21 +36,22 @@ public class LeekArray extends Expression {
 	}
 
 	public void addValue(WordCompiler compiler, Expression key, Token keyToken, Expression value) {
+		verifyDuplicatedKeys(compiler, key, keyToken);
 
-		// Clés dupliquée ?
-		for (int i = 0; i < mValues.size(); i += 2) {
-			if (key.equals(mValues.get(i))) {
-				var level = compiler.getVersion() >= 4 ? AnalyzeErrorLevel.ERROR : AnalyzeErrorLevel.WARNING;
-				compiler.addError(new AnalyzeError(key.getLocation(), level, Error.MAP_DUPLICATED_KEY));
-			}
-		}
+		mElements.add(new LeekMapKeyValue(key, value));
 
 		mIsKeyVal = true;
-		mValues.add(key);
-		mValues.add(value);
-
 		if (compiler.getVersion() >= 4) {
 			type = Type.MAP;
+		}
+	}
+
+	private void verifyDuplicatedKeys(WordCompiler compiler, Expression key, Token keyToken) {
+		for (var element : mElements) {
+			if (element instanceof LeekMapKeyValue && key.equals(((LeekMapKeyValue) element).getKey())) {
+				var level = compiler.getVersion() >= 4 ? AnalyzeErrorLevel.ERROR : AnalyzeErrorLevel.WARNING;
+				compiler.addError(new AnalyzeError(keyToken.getLocation(), level, Error.MAP_DUPLICATED_KEY));
+			}
 		}
 	}
 
@@ -66,77 +67,65 @@ public class LeekArray extends Expression {
 
 	@Override
 	public String toString() {
-		if (mIsKeyVal && mValues.size() == 0) return "[:]";
+		if (mIsKeyVal && mElements.size() == 0)
+			return "[:]";
+
 		String str = "[";
-		for (int i = 0; i < mValues.size(); i++){
-			if (i > 0) str += ", ";
-			if (mIsKeyVal) {
-				str += mValues.get(i).toString() + ": ";
-				i++;
-			}
-			str += mValues.get(i).toString();
+		var first = true;
+		for (var element : mElements) {
+			if (!first)
+				str += ", ";
+			str += element.toString();
+			first = false;
 		}
 		return str + "]";
 	}
 
 	@Override
 	public boolean validExpression(WordCompiler compiler, MainLeekBlock mainblock) throws LeekExpressionException {
-		for (Expression parameter : mValues) {
-			parameter.validExpression(compiler, mainblock);
+		var allValid = true;
+		for (var element : mElements) {
+			allValid = allValid && element.validExpression(compiler, mainblock);
 		}
-		return true;
+		return allValid;
 	}
 
 	@Override
 	public void preAnalyze(WordCompiler compiler) {
-		for (var value : mValues) {
-			value.preAnalyze(compiler);
+		for (var element : mElements) {
+			element.preAnalyze(compiler);
 		}
 	}
 
 	@Override
 	public void analyze(WordCompiler compiler) {
 		operations = 0;
-		for (var value : mValues) {
-			value.analyze(compiler);
-			operations += 2 + value.getOperations();
+		for (var element : mElements) {
+			element.analyze(compiler);
+			operations += element.getOperations();
 		}
 	}
 
 	@Override
 	public void writeJavaCode(MainLeekBlock mainblock, JavaWriter writer) {
-		if (mainblock.getVersion() >= 4) {
-			if (mIsKeyVal) {
-				if (mValues.size() == 0) writer.addCode("new MapLeekValue(" + writer.getAIThis() + ")");
-				else {
-					writer.addCode("new MapLeekValue(" + writer.getAIThis() + ", new Object[] { ");
-					for (int i = 0; i < mValues.size(); i++) {
-						if (i != 0) writer.addCode(", ");
-						mValues.get(i).writeJavaCode(mainblock, writer);
-					}
-					writer.addCode(" })");
-				}
-			} else {
-				if (mValues.size() == 0) writer.addCode("new ArrayLeekValue(" + writer.getAIThis() + ")");
-				else {
-					writer.addCode("new ArrayLeekValue(" + writer.getAIThis() + ", new Object[] { ");
-					for (int i = 0; i < mValues.size(); i++) {
-						if (i != 0) writer.addCode(", ");
-						mValues.get(i).writeJavaCode(mainblock, writer);
-					}
-					writer.addCode(" })");
-				}
-			}
+		boolean passIsKeyValAsParameter = mainblock.getVersion() < 4;
+		String klass = mainblock.getVersion() < 4 ? "LegacyArrayLeekValue"
+				: (mIsKeyVal ? "MapLeekValue" : "ArrayLeekValue");
+
+		writer.addCode("new " + klass + "(" + writer.getAIThis() + ", new Object[] { ");
+
+		boolean first = true;
+		for (var element : mElements) {
+			if (!first)
+				writer.addCode(", ");
+			element.writeJavaCode(mainblock, writer);
+			first = false;
+		}
+
+		if (passIsKeyValAsParameter) {
+			writer.addCode(" }, " + (mIsKeyVal ? "true" : "false") + ")");
 		} else {
-			if (mValues.size() == 0) writer.addCode("new LegacyArrayLeekValue()");
-			else {
-				writer.addCode("new LegacyArrayLeekValue(" + writer.getAIThis() + ", new Object[] { ");
-				for (int i = 0; i < mValues.size(); i++) {
-					if (i != 0) writer.addCode(", ");
-					mValues.get(i).writeJavaCode(mainblock, writer);
-				}
-				writer.addCode(" }, " + (mIsKeyVal ? "true" : "false") + ")");
-			}
+			writer.addCode(" })");
 		}
 	}
 
@@ -148,7 +137,96 @@ public class LeekArray extends Expression {
 	@Override
 	public Hover hover(Token token) {
 		var hover = new Hover(getType(), getLocation(), toString());
-		hover.setSize(mIsKeyVal ? mValues.size() / 2 : mValues.size());
+		hover.setSize(mElements.size());
 		return hover;
+	}
+
+	private abstract class LeekArrayElement {
+		public abstract String toString();
+
+		public abstract void preAnalyze(WordCompiler compiler);
+
+		public abstract void analyze(WordCompiler compiler);
+
+		public abstract int getOperations();
+
+		public abstract boolean validExpression(WordCompiler compiler, MainLeekBlock mainblock)
+				throws LeekExpressionException;
+
+		public abstract void writeJavaCode(MainLeekBlock mainblock, JavaWriter writer);
+	}
+
+	private final class LeekArrayValue extends LeekArrayElement {
+		private final Expression value;
+
+		private LeekArrayValue(Expression value) {
+			this.value = value;
+		}
+
+		public String toString() {
+			return value.toString();
+		}
+
+		public void preAnalyze(WordCompiler compiler) {
+			value.preAnalyze(compiler);
+		}
+
+		public void analyze(WordCompiler compiler) {
+			value.analyze(compiler);
+		}
+
+		public int getOperations() {
+			return 2 + value.getOperations();
+		}
+
+		public boolean validExpression(WordCompiler compiler, MainLeekBlock mainblock) throws LeekExpressionException {
+			return value.validExpression(compiler, mainblock);
+		}
+
+		public void writeJavaCode(MainLeekBlock mainblock, JavaWriter writer) {
+			value.writeJavaCode(mainblock, writer);
+		}
+	}
+
+	private final class LeekMapKeyValue extends LeekArrayElement {
+		private final Expression key;
+		private final Expression value;
+
+		private LeekMapKeyValue(Expression key, Expression value) {
+			this.key = key;
+			this.value = value;
+		}
+
+		public Expression getKey() {
+			return key;
+		}
+
+		public String toString() {
+			return key.toString() + ": " + value.toString();
+		}
+
+		public void preAnalyze(WordCompiler compiler) {
+			key.preAnalyze(compiler);
+			value.preAnalyze(compiler);
+		}
+
+		public void analyze(WordCompiler compiler) {
+			key.analyze(compiler);
+			value.analyze(compiler);
+		}
+
+		public int getOperations() {
+			return 4 + key.getOperations() + value.getOperations();
+		}
+
+		public boolean validExpression(WordCompiler compiler, MainLeekBlock mainblock) throws LeekExpressionException {
+			return key.validExpression(compiler, mainblock) && value.validExpression(compiler, mainblock);
+		}
+
+		public void writeJavaCode(MainLeekBlock mainblock, JavaWriter writer) {
+			key.writeJavaCode(mainblock, writer);
+			writer.addCode(", ");
+			value.writeJavaCode(mainblock, writer);
+		}
 	}
 }
